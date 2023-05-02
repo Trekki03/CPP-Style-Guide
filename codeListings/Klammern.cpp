5f3a244151b4f0f6a174c4f2e79ab8c96e562ac0
Trekki03 <33525119+Trekki03@users.noreply.github.com>
2023-05-02 10:48:55 +0200

    Initial Commit

---
+++ codeListings/Klammern.cpp
@@ -0,0 +1,16 @@
+int main()
+{   // Alleingestellte Klammer
+    int input;
+    std::cin >> input;
+
+    // Guard Clause
+    if(4 == input) {return 1}   
+
+    // Code here
+
+    return 0;
+}   // Alleingestellte Klammer
+
+Car::Car()
+: color(255), wheelCount(4)
+{}  // Leere Funktion
